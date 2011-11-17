3bf9d622533ed5343426e93224fb1bcc22dc0cd5
Michael Alexander <beefsack@gmail.com>
2011-11-17 18:40:13 +1100

    HTTP auth working.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   respond_to :json, :xml
+  before_filter :authenticate_user!
   def index
     respond_with(@users = User.all)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111117062406) do
+ActiveRecord::Schema.define(:version => 20111117072659) do
 
   create_table "cards", :force => true do |t|
     t.text     "front"
@@ -68,7 +68,6 @@ ActiveRecord::Schema.define(:version => 20111117062406) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email",                                 :default => "", :null => false
