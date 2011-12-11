c0fe7b03b011ca4e99c2e9c4d1ff753418b8288c
Michael Alexander <beefsack@gmail.com>
2011-12-11 18:44:46 +1100

    Updated resource methods and model validators for deck card.

--- app/controllers/deck_cards_controller.rb
+++ app/controllers/deck_cards_controller.rb
@@ -1,2 +1,36 @@
 class DeckCardsController < ApplicationController
+  def index
+    conditions = {}
+    conditions[:deck_id] = params[:deck_id] unless params[:deck_id].nil?
+    conditions[:card_id] = params[:card_id] unless params[:card_id].nil?
+    respond_with(@deck_cards = DeckCard.all(:conditions => conditions))
+  end
+  
+  def create
+    @deck_card = DeckCard.new(params[:deck_card])
+    if @deck_card.save
+      redirect_to @deck_card
+    else
+      respond_with(@deck_card.errors, :status => :bad_request, :location => nil)
+    end
+  end
+  
+  def show
+    respond_with(@deck_card = DeckCard.find(params[:id]))
+  end
+  
+  def update
+    @deck_card = DeckCard.find(params[:id])
+    if @deck_card.update_attributes(params[:deck_card])
+      redirect_to @deck_card
+    else
+      respond_with(@deck_card.errors, :status => :bad_request, :location => nil)
+    end
+  end
+  
+  def destroy
+    @deck_card = DeckCard.find(params[:id])
+    @deck_card.destroy
+    respond_with(nil)
+  end
 end
--- app/models/deck_card.rb
+++ app/models/deck_card.rb
@@ -1,4 +1,7 @@
 class DeckCard < ActiveRecord::Base
   belongs_to :deck
   belongs_to :card
+  
+  validates :card_id, :presence => true
+  validates :deck_id, :presence => true
 end
