a41b7c9043902e96e19af47e644d70e974a974ef
Michael Alexander <beefsack@gmail.com>
2011-12-11 17:25:18 +1100

    Added validation to card model.

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,7 @@ class Card < ActiveRecord::Base
   has_many :deck_cards
   has_many :decks, :through => :deck_cards
   has_many :user_deck_session_cards
+  
+  validates :front, :presence => true
+  validates :back, :presence => true
 end
