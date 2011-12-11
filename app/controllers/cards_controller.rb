f1b7e32aaecba68f7668fe9b37db9de3ebc17a47
Michael Alexander <beefsack@gmail.com>
2011-12-11 17:25:53 +1100

    Added resource methods for card controller.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,33 @@
 class CardsController < ApplicationController
+  def index
+    respond_with(@cards = Card.all)
+  end
+  
+  def create
+    @card = Card.new(params[:card])
+    if @card.save
+      redirect_to @card
+    else
+      respond_with(@card.errors, :status => :bad_request, :location => nil)
+    end
+  end
+  
+  def show
+    respond_with(@card = Card.find(params[:id]))
+  end
+  
+  def update
+    @card = Card.find(params[:id])
+    if @card.update_attributes(params[:card])
+      redirect_to @card
+    else
+      respond_with(@card.errors, :status => :bad_request, :location => nil)
+    end
+  end
+  
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    respond_with(nil)
+  end
 end
